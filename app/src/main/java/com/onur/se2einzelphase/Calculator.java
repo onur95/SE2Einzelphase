ec0c71fae5fb1eebb1cae2d506288282a92b9686
onur95 <onur95@gmx.at>
2017-03-12 12:11:52 +0100

    Added functionality to check if fields are empty

---
+++ app/src/main/java/com/onur/se2einzelphase/Calculator.java
@@ -0,0 +1,9 @@
+package com.onur.se2einzelphase;
+
+/**
+ * Created by Onur on 12.03.2017.
+ */
+
+public class Calculator {
+
+}
--- app/src/main/java/com/onur/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/onur/se2einzelphase/MainActivity.java
@@ -2,12 +2,32 @@ package com.onur.se2einzelphase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtDividend;
+    EditText txtDivisor;
+    EditText txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtDividend=(EditText)findViewById(R.id.txtDividend);
+        txtDivisor=(EditText)findViewById(R.id.txtDivisor);
+        txtOutput=(EditText)findViewById(R.id.txtOutput);
+    }
+
+    public void calculate (View v){
+        txtOutput.setText("");
+        if(txtDividend.getText().length()==0 || txtDivisor.getText().length()==0){
+            txtOutput.setText("Beide Felder ausfüllen!");
+        }
+        else{
+
+        }
     }
 }
